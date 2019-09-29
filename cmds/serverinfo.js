8d18b8f5f807cea93fdb53ce662419ee4484a48f
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-09-29 16:39:56 +0200

    Create serverinfo.js

---
+++ cmds/serverinfo.js
@@ -0,0 +1,20 @@
+exports.run = (client, message, args) => {
+    const Discord = require('discord.js')
+    const moment = require('moment')
+    var embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('Название', message.guild.name, true)
+    .addField('Создатель', message.guild.owner, true)
+    .addField('Создан', moment(message.guild.createdAt).format('HH:MM DD-MM-YY'), true)
+    .addField('Роли', message.guild.roles.size, true)
+    .addField('Участников', message.guild.members.size, true)
+    .setThumbnail(message.guild.iconURL)
+    .setColor('RANDOM')
+    message.channel.send(embed)
+   }
+   exports.help = {
+    group: 'general',
+    desc: 'Показывает информацию о сервере',
+    usage: 'g!serverinfo',
+    name: 'serverinfo'
+   }
