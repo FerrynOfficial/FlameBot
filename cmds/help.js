2719387e36a90a9859dcd68126df0f76dca7116f
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-10-18 17:32:55 +0200

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,23 +1,15 @@
 
 exports.run = (client, message, args) => {
     const Discord = require('discord.js')
-
+    const moment = require ('moment')
+    
     var embed = new Discord.RichEmbed()
-    .setDescription('Меню помощи было выслано вам в ЛС.')
+    .setTitle('Help Menu / Меню Помощи')
+    .setThubnail('http://pngimg.com/uploads/question_mark/question_mark_PNG87.png')
+    .setDescription(`Hello; This is a test!/nPrefix: ${client.prefix}`)
     .setColor('RANDOM')
     message.channel.send(embed)
 
-    var embed1 = new Discord.RichEmbed()
-    .setTitle('Команды для Администрации')
-    .setDescription(`kick, ban, warn, modinfo/ntest`)
-    .setColor('#FF5733')
-    message.member.send(embed1)
-
-    var embed2 = new Discord.RichEmbed()
-    .setTitle('Команды для Участников')
-    .setDescription('`help, about, ping, serverinfo`')
-    .setColor('#27AE60')
-    message.member.send(embed2)
 }
 exports.help = {
     group: 'general',
