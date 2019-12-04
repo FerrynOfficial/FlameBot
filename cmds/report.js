4c7545676ed08398a69252daaf6d7de8c2797961
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-04 20:45:33 +0100

    Create report.js

---
+++ cmds/report.js
@@ -0,0 +1,21 @@
+exports.run = (client, message, args) => {
+    const Discord = require('discord.js')
+    const fs = require('fs')
+    var rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.reply('Error')
+    var reason = args.slice.join(' ').slice('22')
+
+    var rEmbed = new Discord.RichEmbed()
+    .setDescription('Reports | Жалобы')
+    .setColor('#FF0000')
+    .addField('Участник',`${rUser}`)
+    .addField('Пожаловался',`${message.author}`)
+    .addField('Канал', message.channel)
+    .addField('Время', message.createdAt)
+    .addField('Причина', reason)
+}
+    var reportsChannel = member.guild.channels.find(channel => channel.id === '651854944306987019')
+    if(!reportsChannel) return message.reply('error')
+
+    message.delete.catch(O_o=>{})
+    reportsChannel.send(rEmbed)
