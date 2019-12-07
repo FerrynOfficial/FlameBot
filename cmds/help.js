c7d3a2aca236734168758c0cf7ecbfdc812da8ed
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-07 10:20:35 +0100

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,21 +1,14 @@
 
 exports.run = (client, message, args) => {
     const Discord = require('discord.js')
-    const moment = require ('moment')
-
+    const fs = require('fs')
+    const moment = require('moment')
     var embed = new Discord.RichEmbed()
-    .setTitle('Help Menu / Меню Помощи')
-    .setThumbnail('http://pngimg.com/uploads/question_mark/question_mark_PNG87.png')
-    .setDescription(`Привет, я GameErr.Bot! Если ты воспользовался командой помощи, значит ты хотел получить всю информацию обо мне. Давай начнём! 
-    Я бот сервера TheGamers и разработан исключительно для него. На данный момент у меня не так уж мни много команд, но ты это можешь исправить, кинув идею в лс @TheFerryn™#0775.\nПрефикс: ${client.prefix}`)
+    .setTitle('Справочник Команд')
     .setColor('RANDOM')
-    .setFooter('Создатель: @TheFerryn | Создан: 28.09.2019')
+    .setDescription('Основные Команды\n`serverinfo`- Показывает информацию о сервере')
     message.channel.send(embed)
-
 }
 exports.help = {
-    group: 'general',
-    desc: 'Выдает меню помощи',
-    usage: 'g!help',
-    name:'help'
- }
+    name: 'help'
+}
