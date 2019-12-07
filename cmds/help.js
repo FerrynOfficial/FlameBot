2c47815004764cede2e6d0b9c219594fde4ac79f
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-07 10:36:29 +0100

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -4,10 +4,15 @@ exports.run = (client, message, args) => {
     const fs = require('fs')
     const moment = require('moment')
     var embed = new Discord.RichEmbed()
-    .setTitle('Справочник Команд')
+    .setTitle('**Справочник Команд**')
     .setColor('RANDOM')
-    .setDescription('Основные Команды\n`serverinfo`- Показывает информацию о сервере')
-    message.channel.send(embed)
+    .setDescription('Основные Команды\n`serverinfo`- Показывает информацию о сервере\n`about`- Показывает информацию о боте\n`ping`- Показывает ваш пинг\n`report`- Пожаловаться на участника\nКоманды для Модераторов\n`kick`- Кикает участника\n`ban`- Банит участника\n`warn`- Выдает предупреждение')
+    .setFooter('В скором времени будет добавлено больше команд!')
+    message.member.send(embed)
+    var embed2 = new Discord.RichEmbed()
+    .setDescription(':tick:Меню помощи было выслано вам в Личные Сообщения')
+    .setColor('RANDOM')
+    message.channel.send(embed2)
 }
 exports.help = {
     name: 'help'
