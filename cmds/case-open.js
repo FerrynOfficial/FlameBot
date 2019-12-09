20f5ce9cdb7afb53f2afa434be9965746d28b1db
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-09 16:12:59 +0100

    Update case-open.js

--- cmds/case-open.js
+++ cmds/case-open.js
@@ -1,9 +1,8 @@
 exports.run = (client, message, args) => {
     const Discord = require('discord.js')
     const fs = require('fs')
-    var caseRole = message.guild.roles.find(`name`, 'DonateCase')
-    if(!caseRole) return message.reply('error')
-    if(!message.member.caseRole) return message.reply('У вас нету кейсов для открития!')
+    var caseRole = message.member.roles.get('653581785929875475')
+    if(!caseRole) return message.reply('У вас нету кейсов для открития!')
     var replies = [`Поздравляем вас ${member}, вы выиграли **Prime!**`,`Поздравляем вас ${member}, вы выиграли **Prime!**`,`Поздравляем вас ${member}, вы выиграли **Prime!**`,`Поздравляем вас ${member}, вы выиграли **Prime!**`,`Поздравляем вас ${member}, вы выиграли **Prime!**`,`Поздравляем вас ${member}, вы выиграли **Premium**`,`Поздравляем вас ${member}, вы выиграли **Premium**`,`Поздравляем вас ${member}, вы выиграли **Premium**`,`Поздравляем вас ${member}, вы выиграли **Premium**`,`Поздравляем вас ${member}, вы выиграли **Premium**`,`Поздравляем вас ${member}, вы выиграли **Premium+**`,`Поздравляем вас ${member}, вы выиграли **Premium+**`,`Поздравляем вас ${member}, вы выиграли **Premium+**`,`Поздравляем вас ${member}, вы выиграли **Deluxe**`,`Поздравляем вас ${member}, вы выиграли **Deluxe**`,`Поздравляем вас ${member}, вы выиграли **Titan!**`]
     var result = Math.floor((Math.random() * replies.length));
     var embed = new Discord.RichEmbed()
