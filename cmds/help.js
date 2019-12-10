2bba1dd25f0a188bf6e753aba09c1b6d86d497c9
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-10 16:11:22 +0100

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
     .setColor('RANDOM')
     .setDescription('**Основные Команды**\n\n`serverinfo`- Показывает информацию о сервере\n`about`- Показывает информацию о боте\n`ping`- Показывает ваш пинг\n`report`- Пожаловаться на участника\n\n**Команды для Модераторов**\n\n`kick`- Кикает участника\n`ban`- Банит участника\n`warn`- Выдает предупреждение')
     .addField('Развлекательные Команды\n\n`8Ball`- Дает ответ на любой вопрос')
-    .setFooter('В скором времени будет добавлено больше команд!')
+    .setFooter('В скором времени будет добавлено больше команд!', true)
     message.member.send(embed)
     var embed2 = new Discord.RichEmbed()
     .setDescription(' Меню помощи было выслано вам в личные сообщения')
