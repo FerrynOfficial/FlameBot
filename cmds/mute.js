74d84a52a906c5fb5a71d0e5abe01be4089c35ec
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-10 21:28:39 +0100

    Update mute.js

--- cmds/mute.js
+++ cmds/mute.js
@@ -1,4 +1,4 @@
-exports.run = (await,client, message, args) => {
+exports.run = (client, message, args) => {
     const Discord = require('discord.js')
     const ms = require('ms')
     if(!message.member.hasPermission('MANAGE_MEMBERS')) return message.reply('У вас нет прав!')
