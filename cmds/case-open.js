ceb4d229fd2c4a6bfed8b535c0fec3aceac0a609
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-10 18:02:57 +0100

    Update case-open.js

--- cmds/case-open.js
+++ cmds/case-open.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
     const Discord = require('discord.js')
     const fs = require('fs')
     var caseRole = message.member.roles.get('653581785929875475')
-    if(!caseRole) return message.reply('У вас нету кейсов для открития!')
+    if(!caseRole) return message.reply('У вас нету кейсов для открытия!')
     var replies = [`Поздравляем вас, вы выиграли **Prime!**`,`Поздравляем ваc, вы выиграли **Prime!**`,`Поздравляем вас, вы выиграли **Prime!**`,`Поздравляем вас, вы выиграли **Prime!**`,`Поздравляем вас, вы выиграли **Prime!**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium+**`,`Поздравляем вас, вы выиграли **Premium+**`,`Поздравляем вас, вы выиграли **Premium+**`,`Поздравляем вас, вы выиграли **Deluxe**`,`Поздравляем вас, вы выиграли **Deluxe**`,`Поздравляем вас, вы выиграли **Titan!**`]
     var result = Math.floor((Math.random() * replies.length));
     var embed = new Discord.RichEmbed()
