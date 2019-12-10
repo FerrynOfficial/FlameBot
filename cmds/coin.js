d4a8e349c97941217c982ea9e7fa0b8c33f38ae1
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-10 18:01:22 +0100

    Create coin.js

---
+++ cmds/coin.js
@@ -0,0 +1,13 @@
+exports.run = (client, message, args) => {
+    const Discord = require('discord.js')
+    var replies = ['Орёл', 'Решка']
+    var result = Math.floor((Math.random() * replies.length));
+    var embed = new Discord.RichEmbed()
+    .setDescription('Вам выпал(а)',replies[result])
+    .setColor('FFFF00')
+    .setThumbnail('https://cdn.discordapp.com/attachments/627452935676100618/654003626942922752/coin_PNG36943.png')
+    message.channel.send(embed)
+}
+exports.help = {
+    name: 'coin'
+}
