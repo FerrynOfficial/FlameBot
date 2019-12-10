c8b51165126f2f26d6522025a2ee310fb76a5d0a
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-10 18:07:38 +0100

    Update coin.js

--- cmds/coin.js
+++ cmds/coin.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     var replies = ['Орёл', 'Решка']
     var result = Math.floor((Math.random() * replies.length));
     var embed = new Discord.RichEmbed()
-    .setDescription('Вам выпал(а)',replies[result])
+    .addField('Вам выпал(а)',replies[result])
     .setColor('FFFF00')
     .setThumbnail('https://cdn.discordapp.com/attachments/627452935676100618/654003626942922752/coin_PNG36943.png')
     message.channel.send(embed)
