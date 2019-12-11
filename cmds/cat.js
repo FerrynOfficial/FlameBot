780ebdd63c6cd78a30e8dfdc8be1ecb5a4d2f73b
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-11 18:15:21 +0100

    Update cat.js

--- cmds/cat.js
+++ cmds/cat.js
@@ -1,12 +1,11 @@
 exports.run = async (client, message, args) => {
     const Discord = require('discord.js')
     const superagent = require('superagent')
-    const { body } = await superagent
-    .get('http://random.cat/meow')
-    var embed = new Discord.RichEmbed()
+    var {body} = await superagent.get('http://random.cat/meow')
+    var cat = new Discord.RichEmbed()
     .setColor('RANDOM')
     .setImage(body.file)
-    message.channel.send({embed})
+    message.channel.send(cat)
 }
 exports.help = {
     name: 'cat'
