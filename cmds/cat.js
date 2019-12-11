2cbfa86706689f922beed15929fc5036922e88a1
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-11 17:51:58 +0100

    Create cat.js

---
+++ cmds/cat.js
@@ -0,0 +1,13 @@
+exports.run = async (client, message, args) => {
+    const Discord = require('discord.js')
+    const superagent = require('superagent')
+    const { body } = await superagent
+    .get('http://random.cat/meow')
+    var embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setImage(body.file)
+    message.channel.send({embed})
+}
+exports.help = {
+    name: 'cat'
+}
