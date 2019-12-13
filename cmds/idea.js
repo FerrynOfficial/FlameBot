700df4fd6f9d88312731e5af112691ec611a215a
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-13 22:50:59 +0100

    Update idea.js

--- cmds/idea.js
+++ cmds/idea.js
@@ -1,11 +1,12 @@
 exports.run = (client, message, args) => {
     const Discord = require('discord.js')
     var idea = args.slice(1).join(' ')
+    if(!idea) return message.reply('Вы не указали идею!')
     var embed = new Discord.RichEmbed()
     .setTitle('Идея')
     .setDescription(idea)
     .setColor('RANDOM')
-    .setFooter(message.author.username, messsage.author.avatarURL)
+    .setFooter(message.author.username, message.author.avatarURL)
     message.channel.send(embed)
     message.react(':thumbsup:')
     message.react(':thumbsdown:')
