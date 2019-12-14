bb619a3572c853895ad7febbbf3b8e5fe29d7c9c
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-14 12:32:39 +0100

    Update idea.js

--- cmds/idea.js
+++ cmds/idea.js
@@ -10,7 +10,8 @@ exports.run = (client, message, args) => {
     message.channel.send({ embed }).then((message) => {
         message.react('👍')
             .then(() => message.react('👎'))
-    }
+        });
+    
 }
 exports.help = {
     name: 'idea'
