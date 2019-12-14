c899ea2ba11f3bdb1739b16b9bff3bc2c63319ee
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-14 17:51:49 +0100

    Create giveaway.js

---
+++ cmds/giveaway.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, args) => {
+    const Discord = require('discord.js')
+    var present =  args.slice(0).join(' ')
+    var giveaway = args.slice(1).join(' ')
+    if(!message.member.permissions.has('ADMINISTRATOR')) return message.reply('У вас недостаточно прав!')
+    var embed = new Discord.RichEmbed()
+    .setTitle('🎉Раздача')
+    .setDescription(giveaway)
+    .addField('Приз', present, true)
+    .setFooter('Для того что бы участвовать, нажми на реакцию ниже!')
+    message.channel.send({ embed }).then((message) => {
+        message.react('👍')
+    });
+}
+exports.help = {
+    name: 'giveaway'
+}
