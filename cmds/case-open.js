e3f7936d1ae3167a36c803bc5b706605d6d78258
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-21 14:14:58 +0100

    Update case-open.js

--- cmds/case-open.js
+++ cmds/case-open.js
@@ -1,6 +1,7 @@
 exports.run = (client, message, args) => {
     const Discord = require('discord.js')
     const fs = require('fs')
+    var author = message.author
     var caseRole = message.member.roles.get('653581785929875475')
     if(!caseRole) return message.reply('У вас нету кейсов для открытия!')
     var replies = [`Поздравляем вас, вы выиграли **Prime!**`,`Поздравляем ваc, вы выиграли **Prime!**`,`Поздравляем вас, вы выиграли **Prime!**`,`Поздравляем вас, вы выиграли **Prime!**`,`Поздравляем вас, вы выиграли **Prime!**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium**`,`Поздравляем вас, вы выиграли **Premium+**`,`Поздравляем вас, вы выиграли **Premium+**`,`Поздравляем вас, вы выиграли **Premium+**`,`Поздравляем вас, вы выиграли **Deluxe**`,`Поздравляем вас, вы выиграли **Deluxe**`,`Поздравляем вас, вы выиграли **Titan!**`]
@@ -11,6 +12,12 @@ exports.run = (client, message, args) => {
     .setDescription(replies[result])
     .setThumbnail('https://cdn.discordapp.com/attachments/627452935676100618/653619763783925810/image0.png')
     message.channel.send(embed).then(message.member.removeRole(caseRole))
+    var embed2 = new Discord.RichEmbed()
+    .setTitle('Донат Кейс')
+    .setDescription(`Пользователь ${author} открыл донат кейс и выиграл ${replies[result]}.`)
+    var logs = message.guild.channels.get('633216142286127114')
+    if(!logs) return message.reply('Error')
+    logs.channel.send(embed2)
 }
 exports.help = {
     name: 'open-case'
