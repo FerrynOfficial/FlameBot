1167595b65de4f364edf90d1f2fb32241d7f3a53
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-23 16:43:13 +0100

    Update addCase.js

--- cmds/addCase.js
+++ cmds/addCase.js
@@ -2,12 +2,11 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 let avtor = message.mentions.users.first() || message.author;
 let role = message.guild.roles.find(r => r.name === "DonateCase");
-    avtor.addRole(role);
     let result = new Discord.RichEmbed()
     .setTitle('Выдача роли')
     .addField('Подробнее:',`Участнику ${avtor} выдали роль ${role}`)
     .setColor('#00ffff')
-message.channel.send(result)
+message.channel.send(result).then(avtor.addRole(role))
 }
 exports.help = {
     name: 'addrole'
