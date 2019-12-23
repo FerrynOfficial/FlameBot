c687e86427e15edabdf228bd7fd646c4e2bf8fbb
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-23 16:41:03 +0100

    Update addCase.js

--- cmds/addCase.js
+++ cmds/addCase.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 let avtor = message.mentions.users.first() || message.author;
-let role = member.guild.roles.find(r => r.name === "DonateCase");
+let role = message.guild.roles.find(r => r.name === "DonateCase");
     avtor.addRole(role);
     let result = new Discord.RichEmbed()
     .setTitle('Выдача роли')
