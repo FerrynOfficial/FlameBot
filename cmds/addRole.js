b4b14739c8e868b48601e8bc52819db77d623374
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-23 17:46:34 +0100

    Update addRole.js

--- cmds/addRole.js
+++ cmds/addRole.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 module.exports.run = async (client,message,args) => {
 var role = message.mentions.roles.first();
 var member = message.mentions.members.first();
+if(!message.member.hasPermission('MANAGE_MEMBERS')) return message.reply('У вас нет прав!')
 var arg = new Array()
 arg[0] = member
 arg[1] = role
@@ -9,7 +10,8 @@ if(!arg[0]) return message.channel.send('Укажите участника')
 if(!arg[1])return message.channel.send('Укажите роль')
     let embed = new Discord.RichEmbed()
     .setTitle('Выдача роли')
-    .addField('Подробнее:',`Участнику ${member} выдали роль ${role}`)
+    .addField('Подробнее:',`**Участнику ${member} выдали роль ${role}**`)
+    .addField('Администратор', `${message.author}`, true)
     .setColor('FF0000')
 message.channel.send(embed)
 await(member.addRole(role))
