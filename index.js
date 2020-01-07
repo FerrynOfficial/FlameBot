f19ab7fcd10f5df49e1f5abc0ea76ddfb3404da0
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2020-01-07 19:05:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,13 +45,6 @@ fs.readdir('./cmds/', (err, files) => {
     })
 })
 client.on('message', message => {
-    
-    if(message.content.toLowerCase() || message.content.toUpperCase() === 'Привет')
-        message.reply('qq')
-    return
-    
-});
-client.on('message', message => {
     let msg = message.content.toLowerCase() || message.content.toUpperCase()
     if(!msg.startsWith(client.prefix)) return;
     if(message.author.bot) return;
