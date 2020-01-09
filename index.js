9bbd1f364748c635f7c38ac43d95e4242548daaf
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2020-01-09 11:30:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ client.on('ready', () => {
         client.user.setActivity('на FlameCommunity 2.0 | Слушает f!help', {url: 'https://twitch.tv/username'})
     }
 })
-client.on('message', message => {
-    if(message.content.toLowerCase() || message.content.toUpperCase() === 'L&D')
-    message.react('👍');
-    message.react('👎');
-    return
-})
 client.prefix = 'f!'
 client.commands = new Discord.Collection();
 fs.readdir('./cmds/', (err, files) => {
