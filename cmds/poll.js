cc80705c1cc9204cb0926c1531c605298a444991
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2020-01-16 15:12:55 +0100

    Update poll.js

--- cmds/poll.js
+++ cmds/poll.js
@@ -1,24 +1,19 @@
+
+
 exports.run = (client, message, args) => {
     const Discord = require('discord.js')
-    const ms = require('ms')
-    var poll = args.slice(1).join(' ')
-    var pollTime = args[0]
-    if(!pollTime) return 
+    var poll = args.slice(0).join(' ')
     if(!poll) return message.reply('Укажите вопрос!')
-    var embed1 = new Discord.RichEmbed()
-    var embed = new Discord.RichEmbed() 
-
+    let embed = new Discord.RichEmbed()
     .setTitle('📊Опрос / Голосование')
     .setDescription(poll)
-    .setFooter(`Опрос от ${message.author.username} | Осталось: ${pollTime}`)
+    .setColor('RANDOM')
+    .setFooter('Что бы проголосовать, нажмите на одну из реакций ниже!')
     message.channel.send({ embed }).then((message) => {
         message.react('👍')
             .then(() => message.react('👎'))
         });
-        setTimeout(function () {
-            embed1.setDescription('Голосование окончено!')
-            message.channel.send(embed1)
-        })
+    message.delete()
 }
 exports.help = {
     name: 'poll'
