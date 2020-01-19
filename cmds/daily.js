528dde37df20fcf6a65238e3284251ce002b4338
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2020-01-19 12:08:32 +0100

    Update daily.js

--- cmds/daily.js
+++ cmds/daily.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args, config) => {
     const Discord = require("discord.js")
     const db = require("quick.db")
     const ms = require("ms")
-    const user = message.mentions.members.first() || message.author
+    const user = message.author
     const money = await db.fetch(`money_${user.id}`)
     
     let timeout = 86400000
@@ -30,12 +30,11 @@ exports.run = async (client, message, args, config) => {
     else {
         let embed = new Discord.RichEmbed()
         .setAuthor(message.author.username, message.author.avatarURL)
-        .setColor("006400")
         .setDescription(`**Ежедневная награда**`)
         .addField("Собрано", amount, true)
         message.channel.send(embed).catch(() => {});
         
-        db.add(`money_${user.id}, amount`)
+        db.add(`money_${user.id}, 500`)
         db.set(`daily_${user.id}`, Date.now())
     }
 }
