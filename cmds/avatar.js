fdd101996433c86fbea814b6b95eeb60054205b2
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2020-01-24 21:43:40 +0100

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -3,7 +3,6 @@ exports.run = (client, message, args) => {
     let user = message.mentions.users.first() || message.author;
     let embed = new Discord.RichEmbed()
     .setColor('RANDOM')
-    .setDescription(user.tag)
     .setImage(`${user.displayAvatarURL}?size=2048`)
     message.channel.send(embed)
 }
