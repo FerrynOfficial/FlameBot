e58e329e451d62feb1937d6c7ec7a8a3ef9f035c
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2020-02-01 14:34:11 +0100

    Update unmute.js

--- cmds/unmute.js
+++ cmds/unmute.js
@@ -2,23 +2,59 @@ exports.run = async(client, message, args) => {
     const Discord = require('discord.js')
     var embed = new Discord.RichEmbed()
     var muser = message.mentions.members.first()
-    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('У вас нет прав!')
+    const muterole = message.guild.roles.find(`name`, 'Muted')
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) {
+        let warn = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setDescription("У вас недостаточно прав для выполнения данного действия!")
+        .setColor('FF0000')
+        message.channel.send(warn)
+        return
+    }
     if(!muser) return message.reply('Укажите пользователя которого хотите размутить!')
-    if(!muser.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.reply('Данный пользователь имеет права равные Вам!')
-    if(!muser.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.reply('У меня нет прав!')
-    var muterole = message.guild.roles.find(`name`, 'Muted')
-    var logs = message.guild.channels.find(`name`, 'logs')
-    if (!logs) return message.reply('Создайте канал под названием "logs"!')
+    if(!muser.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) {
+        let warn1 = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setDescription("Данный пользователь имеет права равные Вам!")
+        .setColor("FF0000")
+        message.channel.send(warn1)
+        return
+    }
+    if(!muser.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) {
+        let warn2 = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setDescription("У меня недостаточно прав для выполнения данного действия!")
+        .setColor('FF0000')
+        message.channel.send(warn2)
+        return               
+    }
+    if(!muser.roles.has(muterole)) {
+        let warn3 = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setDescription("Данный пользователь не замучен!")
+        .setColor('FF0000')
+        message.channel.send(warn3)
+        return
+    }
     embed.setAuthor(message.author.username, message.author.avatarURL)
-    embed.setDescription(`**Пользователь ${muser} был размучен!**`)
-    embed.addField('**Модератор**', message.author, true)
-    embed.setColor('006400')
+    embed.setDescription(`Пользователь был успешно размучен!`)
+    embed.setColor('FF0000')
     message.channel.send(embed)
     await muser.removeRole(muterole)
+    var logs = message.guild.channels.find(`name`, 'logs')
+    if (!logs) {
+        let nologs = new Discord.RichEmbed()
+        .setTitle("⚠️**Предупреждение**")
+        .setDescription("Канал logs не обнаружен!")
+        .setColor('FF0000')
+        message.channel.send(nologs)
+        return
+    }
     var embed1 = new Discord.RichEmbed()
     .setTitle('Unmute | Logs')
-    .setDescription(`**Пользователь ${muser} был размучен!**`)
+    .setDescription(`**Пользователь был размучен!**`)
     .addField('**Модератор**', message.author, true)
+    .addField("Пользователь", muser, true)
      logs.send(embed1)
 }
     exports.help = {
