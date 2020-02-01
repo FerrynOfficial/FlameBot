d0f4acf6f2eebdd7de100aa46c68544a899cde1f
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2020-02-01 10:25:22 +0100

    Update verify.js

--- cmds/verify.js
+++ cmds/verify.js
@@ -3,12 +3,13 @@ exports.run = async(client, message, args, tools) => {
     const Discord = require("discord.js")
     const vUser = message.author;
     const vChannel = message.guild.channels.get("672188144921083907")
-    if(!message.channel === vChannel) {
+    const newbie = message.guild.roles.get("619878513041997824")
+    if(message.member.role.has(newbie)) {
         message.delete()
         return
     }
+   
     const verify = message.guild.roles.get("672751848490401799")
-    const newbie = message.guild.roles.get("619878513041997824")
     let embed = new Discord.RichEmbed()
     .setAuthor(message.author.username, message.author.avatarURL)
     .setDescription('Верификация была успешно пройдена! Спасибо что вы с нами!')
